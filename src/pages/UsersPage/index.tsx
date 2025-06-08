5b03024ea15ece2c79b9aee6269e67dc54018262
Gian Fritsche <gian.fritsche@gmail.com>
2025-06-08 16:01:11 -0300

    refactor: simplify UsersPage component and integrate CRUD context

--- src/pages/UsersPage/index.tsx
+++ src/pages/UsersPage/index.tsx
@@ -1,62 +1,13 @@
-import { useMemo } from 'react';
-import { MantineReactTable, MRT_ColumnDef, useMantineReactTable } from 'mantine-react-table';
-
-type User = {
-  id: number;
-  username: string;
-  admin: boolean;
-};
-
-const data: User[] = [
-  {
-    id: 1,
-    username: 'admin',
-    admin: true,
-  },
-  {
-    id: 2,
-    username: 'user',
-    admin: false,
-  },
-  {
-    id: 3,
-    username: 'user2',
-    admin: false,
-  },
-];
+import { CRUDProvider } from '@/contexts/CRUDContext';
+// import UsersForm from './UsersForm';
+// import { UsersTable } from './UsersTable';
 
 export function UsersPage() {
-  const columns = useMemo<MRT_ColumnDef<User>[]>(
-    () => [
-      {
-        accessorKey: 'id',
-        header: 'ID',
-      },
-      {
-        accessorKey: 'username',
-        header: 'Username',
-      },
-      {
-        accessorKey: 'admin',
-        header: 'Admin',
-      },
-    ],
-    []
+  return (
+    <CRUDProvider endpoint="users">
+      {/* <UsersTable />
+      <UsersForm /> */}
+    </CRUDProvider>
   );
-
-  const table = useMantineReactTable({
-    columns,
-    data,
-    mantinePaperProps: {
-      style: {
-        border: 'none',
-      },
-    },
-    enableBottomToolbar: false,
-    enablePagination: false,
-    enableRowVirtualization: true,
-    mantineTableContainerProps: { style: { maxHeight: 'calc(100vh - 128px)' } },
-  });
-
-  return <MantineReactTable table={table} />;
 }
+
